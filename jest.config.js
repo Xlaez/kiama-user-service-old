a57b0acf30f30f8b19f230f5584eb3d5652e2f4b
Andry Rasolofomanana <andry.rasolofomananaa@gmail.com>
2022-09-12 13:34:53 +0300

    fix: fix test config for jest

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,11 @@
-const { pathsToModuleNameMapper } = require('ts-jest/utils');
-const { compilerOptions } = require('./tsconfig.json');
-
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+// jest.config.js
+const { pathsToModuleNameMapper } = require('ts-jest')
+// In the following statement, replace `./tsconfig` with the path to your `tsconfig` file
+// which contains the path mapping (ie the `compilerOptions.paths` option):
+const { compilerOptions } = require('./tsconfig')
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  roots: ['<rootDir>/src'],
-  transform: {
-    '^.+\\.tsx?$': 'ts-jest',
-  },
-  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/src' }),
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/src' } ),
 };
