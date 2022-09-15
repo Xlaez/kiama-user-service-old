9631ed14111d07dc178727aec9ca361cdf2ccee6
Andry Rasolofomanana <andry.rasolofomananaa@gmail.com>
2022-09-15 15:50:38 +0300

    feat: create producer for rabbitMq

---
+++ auth/src/queue/producer.ts
@@ -0,0 +1,38 @@
+import amqp, { Connection } from 'amqplib/callback_api'
+import { AMP_URL } from '@config';
+import { logger, stream } from '@utils/logger';
+
+
+class mqPublisher {
+  public connection: Connection
+  public channel: amqp.Channel
+  public queueName : string
+
+  constructor() {
+    this.queueName = "kiama-auth"
+
+    amqp.connect(AMP_URL, (err, conn) => {
+      if (err) {
+        logger.error(err)
+        throw err
+      }
+      this.connection = conn
+      conn.createChannel((err, ch) => {
+        if (err) {
+          logger.error(err)
+          throw err
+        }
+        this.channel = ch
+        ch.assertQueue(this.queueName, { durable: false })
+      })
+    })
+  }
+
+  public publish = (msg: string) => {
+    this.channel.sendToQueue(this.queueName, Buffer.from(msg), { persistent: true })
+    logger.info(`Message sent to queue: ${msg}`)
+  }
+
+}
+
+export default mqPublisher
